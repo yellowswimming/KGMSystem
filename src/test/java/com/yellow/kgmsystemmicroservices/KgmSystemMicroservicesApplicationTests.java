825005edd0c26fc5b24527ce05f7db08ff080f91
yellow <2695083201@qq.com>
2025-03-16 15:16:21 +0800

    Initial commit

---
+++ src/test/java/com/yellow/kgmsystemmicroservices/KgmSystemMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellow.kgmsystemmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgmSystemMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
